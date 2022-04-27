8ee7682651758a532a956ae1f1a66873a65f0c0c
Abdurahman <adiab03@adm.huddinge.se>
2022-04-27 13:36:29 +0200

    kund halvfärdig

--- admin/kund.php
+++ admin/kund.php
@@ -3,11 +3,10 @@
     include "../connection.php";
     if(isset($_POST['update'])){
         $oldUid = $_POST['oldUid'];
-        $uid = $_POST['uid'];
+        $kundname = $_POST['kundnamn'];
         $name = $_POST['name'];
-        $pass = $_POST['password'];
-        $group = $_POST['group'];
-        $updateSql = "UPDATE `kund` SET `uid`='$uid',`namn`='$name',`password`='$pass',`grupp`='$group' WHERE `uid`='$oldUid'";
+        $password = $_POST['password'];
+        $updateSql = "UPDATE `kund` SET `KundNamn`='$kundname',`namn`='$name',`password`='$pass',`grupp`='$group' WHERE `uid`='$oldUid'";
         $updateRes = mysqli_query($conn, $updateSql);
     }
 ?>
@@ -16,7 +15,7 @@
 <head>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../style2.css?v=<?php echo time();?>">
+    <link rel="stylesheet" href="../style.css?v=<?php echo time();?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -26,23 +25,28 @@
 <body>
     <a href="home.php">Return</a>
     <a href="index.php">Logout</a>
+    <div class="mottFLEX">
             <?php 
             $admSql = "SELECT * FROM kund";
             $admSqlQuery = mysqli_query($conn, $admSql);
             if($admSqlQuery){
                 while($row = mysqli_fetch_assoc($admSqlQuery)){
             ?>
+            <div class="motUI">
             <form class="user" method="post" action="">
                 <input type="hidden" name="oldUid" value="<?php echo $row['KundId']?>">
-                <p>Name: <input type="text" name="name" value="<?php echo $row['KundId']?>"></p>
-                <p>Uid: <input type="text" name="uid" value="<?php echo $row['KundNamn']?>"></p>
-                <p>Password: <input type="password" name="password" value="<?php echo $row['password']?>"></p>
-                <input type="submit" value="Update" class="btn" name="update">
+                <br>Name: <?php echo $row['KundId']?></br>
+                <p>Uid: <input type="text" name="kundnamn" value="<?php echo $row['KundNamn']?>"></p>
+                <p>Password: <input type="password" name="password" value=""></p>
+                <p>Adress: <input type="text" name="adress" value="<?php echo $row['Adress'];?>"></p>
+                <p>Adress: <input type="text" name="adress" value="<?php echo $row['Adress'];?>"></p>
+                <input type="submit" value="Update" class="button2" name="update">
             </form>
             <?php
                 }
             }
             ?>
+            </div>
         </div>
     </section>
 </body>
